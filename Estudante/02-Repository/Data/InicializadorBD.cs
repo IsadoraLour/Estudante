179eaba7e238f718e4829398e6ea128509241f18
IsadoraLour <163910452+IsadoraLour@users.noreply.github.com>
2024-11-01 16:53:41 -0300

    Adicionar arquivos de projeto.

---
+++ Estudante/02-Repository/Data/InicializadorBD.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Data.SQLite;
+
+
+namespace Estudante
+{
+    public class InicializadorBD
+    {
+
+        private const string ConnectionString = "Data Source=Escola.db";
+        public static void Inicializar()
+        {
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+
+
+                connection.Open();
+                string commandoSQL = @"   
+                 CREATE TABLE IF NOT EXISTS Pessoas(
+                 Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 Altura INTERGER NOT NULL,
+                 Peso REAL NOT NULL,
+                 Idade INTERGER NOT NULL,
+                 Nome STRING NOT NULL
+                );";
+            }
+
+        }
+    }
+
+}
---
+++ Estudante/03-Entidades/Estudante.cs
@@ -0,0 +1,11 @@
+﻿namespace Estudante
+{
+    public class Estudante
+    {
+        public int Idade { get; set; }
+        public string Nome { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+    }
+}
